66fac446fd6efba23dda41ba3d160e9c8d995f81
medism14 <medismael14@gmail.com>
2024-04-07 13:29:34 +0200

    Remove print

--- huffman/decompression.py
+++ huffman/decompression.py
@@ -6,16 +6,13 @@ class Decompression:
 
     def __Remove_Padding(self, text):#2
         padded_info = text[:8]         #extrait les 8 premiers bits du texte
-        print(" les 8 premiers bits du texte:",padded_info)
         padding_carractére = int(padded_info,2)         #convertit les 8 bits en entier en base 10
-        print("bits de rembourrage ajoutés à la fin du texte compressé:",padding_carractére)
         text = text[8:]                     #supprime les 8 premiers bits du texte
         text = text[:-1*padding_carractére] #supprime les bits de remplissage de la fin du texte
         return text
     
     
     def __Decoded_Text(self,text):#3
-        print("Reverse Code Dictionary:",self.__dictionnary) 
         current_bit = ''
         decoded_text = ''
         for char in text:
